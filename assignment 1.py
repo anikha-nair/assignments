d3f7d8ee72a7d2a34745a316a20445b874ed9ec2
anikha-nair <123643711+anikha-nair@users.noreply.github.com>
2023-01-27 17:08:18 +0530

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,15 @@
+#by using strings
+name ="python"
+print(name)
+
+message ="i love python"
+print(message)
+
+#by using tuple with integers
+my_tuple = (1, 2, 3)
+print(my_tuple)
+
+#by using nested tuple
+my_tuple = ("mouse", [8, 4, 6], (1, 2, 3))
+print(my_tuple)
+
